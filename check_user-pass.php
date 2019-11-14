355cb0bd05e47141cf1a32cb89a8474547ad6d7a
Salar Houshvand <salarhoushvand@yahoo.com>
2019-11-13 23:23:13 -0500

    add check_user-pass

---
+++ check_user-pass.php
@@ -0,0 +1,16 @@
+<?php
+    $conn       = mysqli_connect('localhost', 'root', '', 'draid');
+    $username   = $_POST['username'];
+    $password   = $_POST['password'];
+    $query      = "select * from patients where username = '$username'";
+    $resultSet  = mysqli_query($conn, $query); //Syntax error: mysqli_query(connection,query);
+    if(mysqli_num_rows($resultSet) > 0){
+        $row    = mysqli_fetch_assoc($resultSet);
+        if($row['password'] == $password){ // if you are using encryption like md5 or anything else then you have to add in this line accordingly
+            header("Location: mainpage.html");
+        }else{
+            echo "Oh No, password not correct!";
+        }
+    }else{
+        echo "Please enter correct email!";
+    }
