308c232057bdb8aa0863e8f66fffa7fc817e9487
Scott Johnson <sjohnson.wvu@gmail.com>
2019-12-03 14:55:45 -0500

    DB connection for heroku

--- check_user-pass.php
+++ check_user-pass.php
@@ -1,5 +1,5 @@
 <?php
-    $conn       = mysqli_connect('localhost', 'root', '', 'draid');
+    $conn       = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'b113d931bd12b9', '5f01e24a', 'heroku_9155751049f1d50');
     $username   = $_POST['username'];
     $password   = $_POST['password'];
     $query      = "select * from patients where username = '$username'";
