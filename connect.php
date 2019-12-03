c256062f45ed9199eddbb2779e144e658aa5c25d
Scott Johnson <sjohnson.wvu@gmail.com>
2019-12-03 14:47:33 -0500

    DB connection for heroku

--- connect.php
+++ connect.php
@@ -6,7 +6,20 @@ $password = $_POST['password'];
 
 //Database connection
 
-$conn = new mysqli('localhost', 'root', '', 'draid');
+// $conn = new mysqli('localhost', 'root', '', 'draid');
+// if($conn->connect_error){
+//     die('connection failed : '.$conn->connect_error);
+// }else{
+//     $stmt = $conn->prepare("insert into patients(email, username, password)
+//     values(?, ?, ?)");
+//     $stmt->bind_param("sss", $email, $username, $password);
+//     $stmt->execute();
+//     header("Location: successful.html");
+//     $stmt->close();
+//     $conn->close();
+
+// SJ - to support heroku depolyment    
+$conn = new mysqli('us-cdbr-iron-east-05.cleardb.net', 'b113d931bd12b9', '5f01e24a', 'heroku_9155751049f1d50');
 if($conn->connect_error){
     die('connection failed : '.$conn->connect_error);
 }else{
